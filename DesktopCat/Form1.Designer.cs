ec90136df4d09c263e4f9ec7d5f6232d30cdbf8c
Gilfoyle <raphael@reichart.org>
2020-02-17 21:00:39 +0100

    Created my first commit for DesktopCat

---
+++ DesktopCat/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DesktopCat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 84);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(146, 84);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ DesktopCat/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopCat
+{
+    public partial class Form1 : Form
+    {
+        static int speedX = -5;
+        static int speedY = 0;
+        static Boolean followCursor = false;
+        static Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.ControlBox = false;
+            this.pictureBox1.Image = DesktopCat.Properties.Resources.walking_cat_drawing_left;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.TransparencyKey = Color.Black;
+            this.BackColor = Color.Black;
+            pictureBox1.Visible = true;
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (this.Left <= 0)
+            {
+                pictureBox1.Image = DesktopCat.Properties.Resources.walking_cat_drawing_right;
+                speedX *= -1;
+                speedY = rand.Next(0, 7)-3;
+            }
+            else if (this.Left + pictureBox1.Width >= Screen.PrimaryScreen.Bounds.Width)
+            {
+                pictureBox1.Image = DesktopCat.Properties.Resources.walking_cat_drawing_left;
+                speedX *= -1;
+                speedY = rand.Next(0, 7) - 3;
+            }
+
+            if (this.Top <= 0)
+            {
+                speedY = rand.Next(0, 7) - 3;
+                speedY *= -1;
+            }
+            else if (this.Top + this.Height >= Screen.PrimaryScreen.Bounds.Width)
+            {
+                speedY = rand.Next(0, 7) - 3;
+                speedY *= -1;
+            }
+
+            if (rand.NextDouble() < 0.02D)
+            {
+                if (rand.NextDouble() < 0.05D)
+                {
+                    speedX *= -1;
+                    if (speedX < 0)
+                    {
+                        pictureBox1.Image = DesktopCat.Properties.Resources.walking_cat_drawing_left;
+                    }
+                    else
+                    {
+                        pictureBox1.Image = DesktopCat.Properties.Resources.walking_cat_drawing_right;
+                    }
+                }
+                speedY = rand.Next(0, 7) - 3;
+            }
+            this.Left += speedX;
+            this.Top += speedY;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
